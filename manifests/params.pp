f8e8dee356edf8ed6f6c9b867dd148f98928ffc1
Massimiliano Adamo <maxadamo@gmail.com>
2018-05-12 09:55:03 +0200

    exclude vim temp files

--- manifests/params.pp
+++ manifests/params.pp
@@ -7,7 +7,7 @@ class lsync_csync2::params {
   $node_hostname = [] # an array of hostnames, mandatory
   $sync_group = 'sync_group'
   $sync_dir = ['/sync1', '/sync2']
-  $sync_exclude = ['*~', '.*', '*.log', '*.pid']
+  $sync_exclude = ['*.swp', '*~', '.*', '*.log', '*.pid']
   $csync2_ssl_key = lookup('csync2_ssl_key')
   $csync2_ssl_cert = lookup('csync2_ssl_cert')
   $csync2_preshared_key = lookup('csync2_preshared_key')
