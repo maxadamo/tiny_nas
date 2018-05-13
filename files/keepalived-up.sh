82a681a98f376ed1ebf22a989cc89afca5289d76
Massimiliano Adamo <maxadamo@gmail.com>
2018-05-13 22:10:54 +0200

    minor

--- files/keepalived-up.sh
+++ files/keepalived-up.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-if ! pgrep -f lsyncd; do
+if ! pgrep -f lsyncd; then
   systemctl restart lsyncd.service
 done
 
