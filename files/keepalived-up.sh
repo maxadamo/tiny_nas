7d6e95c84caf212e8e8047f438c0ff939a8cabc5
Massimiliano Adamo <maxadamo@gmail.com>
2018-05-13 22:11:52 +0200

    typo

--- files/keepalived-up.sh
+++ files/keepalived-up.sh
@@ -3,7 +3,7 @@ export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
 if ! pgrep -f lsyncd; then
   systemctl restart lsyncd.service
-done
+fi
 
 until pgrep -f lsyncd; do
   systemctl restart lsyncd.service
