35cc60d34f613dbec5390e730d7b6fbd5a7326af
Massimiliano Adamo <maxadamo@gmail.com>
2018-05-13 19:03:52 +0200

    fix scripts

--- files/keepalived-up.sh
+++ files/keepalived-up.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
+if ! pgrep -f lsyncd; do
+  systemctl restart lsyncd.service
+done
+
 until pgrep -f lsyncd; do
   systemctl restart lsyncd.service
 done
 
-#systemctl reload nfs.service
+systemctl reload nfs-server.service
