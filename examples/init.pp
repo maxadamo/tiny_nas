05d14efb7b1271789cfac6f68f4eeeeb6b7c38d0
Massimiliano Adamo <maxadamo@gmail.com>
2018-05-19 18:56:06 +0200

    adde pdk

--- examples/init.pp
+++ examples/init.pp
@@ -6,7 +6,7 @@ class { 'tiny_nas':
   nodes_hostname       => ['puppet02.domain.org', 'puppet03.domain.org'],
   nodes_ip4            => ['192.168.0.10', '192.168.0.11'],
   nodes_ip6            => ['2001:798:3::56', '2001:798:3::54'],
-  csync2_ssl_key       => lookup('csync2_ssl_key'),
-  csync2_ssl_cert      => lookup('csync2_ssl_cert'),
-  csync2_preshared_key => lookup('csync2_preshared_key');
+  csync2_ssl_key       => 'csync2_ssl_key',
+  csync2_ssl_cert      => 'csync2_ssl_cert',
+  csync2_preshared_key => 'csync2_preshared_key';
 }
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 describe 'tiny_nas' do
   context 'with default values for all parameters' do
-    it { should contain_class('tiny_nas') }
+    it { is_expected.to contain_class('tiny_nas') }
   end
 end
