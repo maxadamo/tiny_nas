58a0a6b96cd057a0dea4e0083d6157dcac90b0bc
Massimiliano Adamo <maxadamo@gmail.com>
2018-05-20 20:50:21 +0200

    fix gitlab-ci

--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -1,38 +1,43 @@
 require 'spec_helper'
 describe 'tiny_nas' do
   context 'with default values for all parameters' do
-    let(:params) { {
-      'sync_group'           => 'nas',
-      'sync_dir '            => {
-        'moodledata' => {
-          'client_list' => [
-            '83.97.93.24(rw,insecure,async,no_root_squash)',
-            '2001:798:3::12c(rw,insecure,async,no_root_squash)'
-          ],
-          'dir_watch' => 'false'
+    let(:params) do
+      {
+        'sync_group' => 'nas',
+        'sync_dir ' => {
+          'moodledata' => {
+            'client_list' => [
+              '192.168.0.100(rw,insecure,async,no_root_squash)',
+              '192.168.0.101(rw,insecure,async,no_root_squash)',
+              '2001:777:3::12c(rw,insecure,async,no_root_squash)',
+            ],
+            'dir_watch' => 'false',
+          },
+          'puppet_ca' => {
+            'client_list' => [
+              '192.168.0.101(rw,insecure,async,no_root_squash)',
+              '192.168.0.102(rw,insecure,async,no_root_squash)',
+              '2001:777:3::13a(rw,insecure,async,no_root_squash)',
+            ],
+            'dir_watch' => 'false',
+          },
         },
-        'puppet_ca' => {
-          'client_list' => [
-            '83.97.92.62(rw,insecure,async,no_root_squash)',
-            '2001:798:3::56(rw,insecure,async,no_root_squash)'
-          ],
-          'dir_watch' => 'false'
-        }
-      },
-      'nodes_hostnames'      => ['nas01.geant.org', 'nas02.geant.org'],
-      'nodes_ip4'            => ['83.97.93.40', '83.97.93.41'],
-      'nodes_ip6'            => ['2001:798:3::13c', '2001:798:3::13d'],
-      'vip_ip4'              => '83.97.93.46',
-      'vip_ip4_subnet'       => '22',
-      'vip_ip6'              => '2001:798:3::142',
-      'vip_ip6_subnet'       => '64',
-      'csync2_ssl_key'       => 'csync2_ssl_key',
-      'csync2_ssl_cert'      => 'csync2_ssl_cert',
-      'csync2_preshared_key' => 'csync2_preshared_key',
-      'manage_lvm'           => true,
-      'vg_name'              => 'rootvg',
-      'lv_size'              => 3,
-    } }
+        'nodes_hostnames' => ['nas01.example.org', 'nas02.example.org'],
+        'nodes_ip4' => ['192.168.0.10', '192.168.0.11'],
+        'nodes_ip6' => ['2001:777:3::13', '2001:777:3::14'],
+        'vip_ip4' => '192.168.0.12',
+        'vip_ip4_subnet' => '24',
+        'vip_ip6' => '2001:777:3::15',
+        'vip_ip6_subnet' => '64',
+        'csync2_ssl_key' => 'this_is_my_csync2_ssl_key',
+        'csync2_ssl_cert' => 'this_is_my_csync2_ssl_cert',
+        'csync2_preshared_key' => 'this_is_my_csync2_preshared_key',
+        'manage_lvm' => true,
+        'vg_name' => 'rootvg',
+        'lv_size' => 3,
+      }
+    end
+
     it {
       is_expected.to contain_class('tiny_nas')
     }
