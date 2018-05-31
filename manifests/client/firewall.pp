ef3893f9fec2978b7676d5b4eb5ae11bdf677dab
Massimiliano Adamo <maxadamo@gmail.com>
2018-05-31 14:28:01 +0200

    added client firewall

---
+++ manifests/client/firewall.pp
@@ -0,0 +1,23 @@
+# == Class: tiny_nas::client::firewall
+#
+class tiny_nas::client::firewall {
+
+  ['iptables', 'ip6tables'].each | String $provider {
+    firewall {
+      "200 allow outbound UDP to port 111, 2049, 4045, 10050 for provider ${provider}":
+        chain    => 'OUTPUT',
+        action   => accept,
+        provider => $provider,
+        proto    => udp,
+        dport    => [111, 2049, 4045, 10050];
+      "200 allow outbound TCP to port 111, 2049, 4045, 10050 for provider ${provider}":
+        chain    => 'OUTPUT',
+        action   => accept,
+        provider => $provider,
+        proto    => tcp,
+        dport    => [111, 2049, 4045, 10050];
+    }
+  }
+
+}
+# vim:ts=2:sw=2
