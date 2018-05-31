36dffa4be9b82f7cb9588482bddcbd1fbabe9e39
Massimiliano Adamo <maxadamo@gmail.com>
2018-05-31 16:56:54 +0200

    fix client

---
+++ manifests/client/client.pp
@@ -0,0 +1,16 @@
+# Class: tiny_nas::client::client
+#
+#
+class tiny_nas::client::client {
+
+  include ::tiny_nas::client::firewall
+
+  class { '::nfs':
+    server_enabled => false,
+    client_enabled => true,
+  }
+
+}
+
+
+
