be982814429c75add8081c46cf591600856ce676
Massimiliano Adamo <massimiliano.adamo@geant.org>
2018-06-08 12:21:13 +0200

    allow disable nfs server

--- manifests/client/client.pp
+++ manifests/client/client.pp
@@ -1,13 +1,15 @@
 # Class: tiny_nas::client::client
 #
 #
-class tiny_nas::client::client {
+class tiny_nas::client::client (
+  $nfs_server_enabled = false
+) {
 
   include ::tiny_nas::client::firewall
 
   unless defined(Class['::nfs']) {
     class { '::nfs':
-      server_enabled => false,
+      server_enabled => $nfs_server_enabled,
       client_enabled => true,
     }
   }
