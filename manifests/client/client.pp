f3badf81efa4991d5a7b20a0ad5c3cb5970f99ba
Massimiliano Adamo <maxadamo@gmail.com>
2018-06-11 22:08:47 +0200

    allow to disable firewall for client

--- manifests/client/client.pp
+++ manifests/client/client.pp
@@ -3,11 +3,14 @@
 #
 class tiny_nas::client::client (
   $nfs_server_enabled = false,
+  $manage_firewall    = true,
   $ipv6_enabled       = true
 ) {
 
-  class { '::tiny_nas::client::firewall':
-    ipv6_enabled => $ipv6_enabled;
+  if any2bool($manage_firewall) == true {
+    class { '::tiny_nas::client::firewall':
+      ipv6_enabled => $ipv6_enabled;
+    }
   }
 
   unless defined(Class['::nfs']) {
