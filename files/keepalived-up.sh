63443ac85cb50809971b82cc054607c0142766d2
Massimiliano Adamo <maxadamo@gmail.com>
2020-03-15 21:14:54 +0100

    remove unnecessary code inside script

--- files/keepalived-up.sh
+++ files/keepalived-up.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 export PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin
 
-if ! pgrep -f lsyncd; then
-  systemctl restart lsyncd.service
-fi
-
 until pgrep -f lsyncd; do
   systemctl restart lsyncd.service
 done
